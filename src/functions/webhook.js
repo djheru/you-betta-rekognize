7845806e9f0b8cb4bef4548d84848f7d93569581
Philip Damra <pdamra@dealerinspire.com>
2020-01-07 23:02:07 -0700

    Registering, validating, and deregistering webhooks

---
+++ src/functions/webhook.js
@@ -0,0 +1,40 @@
+import { deleteWebhook, createWebhook, generateWebhookResponseToken } from '../services/webhook';
+
+export const register = async event => {
+  console.log(event);
+  const result = await createWebhook();
+  console.log(result);
+  return result
+    ? { statusCode: 200, body: JSON.stringify(result, null, '\t') }
+    : {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Unable to register webook' }),
+      };
+};
+
+export const validate = async event => {
+  console.log(event);
+  const { queryStringParameters = {} } = event;
+  if (!queryStringParameters) {
+    return { statusCode: 200 };
+  }
+  const { crc_token: crcToken = '' } = queryStringParameters;
+  const token = generateWebhookResponseToken(crcToken);
+  return token
+    ? { statusCode: 200, body: token }
+    : {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Unable to validate webook' }),
+      };
+};
+
+export const deregister = async event => {
+  console.log(event);
+  const result = await deleteWebhook();
+  return result
+    ? { statusCode: 200 }
+    : {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Unable to deregister webook' }),
+      };
+};
---
+++ src/services/webhook.js
@@ -0,0 +1,105 @@
+import request from 'request-promise';
+import crypto from 'crypto';
+
+const {
+  API_GATEWAY_URL = '',
+  TWITTER_ACCESS_TOKEN = '',
+  TWITTER_ACCESS_TOKEN_SECRET = '',
+  TWITTER_CONSUMER_API_KEY = '',
+  TWITTER_CONSUMER_API_KEY_SECRET = '',
+  TWITTER_DEV_ENV = '',
+} = process.env;
+
+const oauth = {
+  consumer_key: TWITTER_CONSUMER_API_KEY,
+  consumer_secret: TWITTER_CONSUMER_API_KEY_SECRET,
+  token: TWITTER_ACCESS_TOKEN,
+  token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+};
+
+const TWITTER_WEBHOOK_URL = 'https://api.twitter.com/1.1/account_activity/all';
+
+export const generateWebhookResponseToken = crcToken => {
+  if (crcToken) {
+    const hash = crypto
+      .createHmac('sha256', TWITTER_CONSUMER_API_KEY_SECRET)
+      .update(crcToken)
+      .digest('base64');
+    return JSON.stringify({ response_token: `sha256=${hash}` });
+  }
+  return false;
+};
+
+export const getWebhook = async () => {
+  const url = `${TWITTER_WEBHOOK_URL}/${TWITTER_DEV_ENV}/webhooks.json`;
+  const requestOptions = {
+    url,
+    oauth,
+  };
+  try {
+    const response = await request.get(requestOptions);
+    const responseData = JSON.parse(response);
+    if (!responseData.length) {
+      console.log('No existing webhook found');
+      return false;
+    }
+    console.log('getWebhook', responseData);
+    return responseData[0];
+  } catch (e) {
+    console.log('getWebhook err', e);
+    return false;
+  }
+};
+
+export const createWebhook = async () => {
+  const url = `${TWITTER_WEBHOOK_URL}/${TWITTER_DEV_ENV}/webhooks.json`;
+  const processingUrl = `${API_GATEWAY_URL}/webhook/process`;
+  const requestOptions = {
+    url,
+    oauth,
+    resolveWithFullResponse: true,
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded',
+    },
+    form: { url: processingUrl },
+  };
+
+  try {
+    // If webhook is already registered, use it
+    const existingWebhook = await getWebhook();
+    if (existingWebhook && existingWebhook.url === processingUrl) {
+      console.log('existing webhook', existingWebhook);
+      return existingWebhook;
+    }
+    await request.post(requestOptions);
+    const webhookResponse = await getWebhook();
+    console.log('createWebhook', { webhookResponse });
+    return webhookResponse;
+  } catch (e) {
+    console.log('createWebhook err', e.message || 'Error registering webhook');
+    return false;
+  }
+};
+
+export const deleteWebhook = async () => {
+  try {
+    const webhookResponse = await getWebhook();
+    if (!webhookResponse) {
+      // If no webhook exists, we're good
+      return true;
+    }
+    console.log('Existing webhookResponse', webhookResponse);
+    const { id: webhookId } = webhookResponse;
+    const url = `${TWITTER_WEBHOOK_URL}/${TWITTER_DEV_ENV}/webhooks/${webhookId}.json`;
+    const requestOptions = {
+      url,
+      oauth,
+    };
+    const response = await request.delete(requestOptions);
+    console.log('deleteWebhook', response);
+    return true;
+  } catch (e) {
+    console.log('deleteWebhook err', e);
+    return false;
+  }
+};
